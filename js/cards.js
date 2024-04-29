f43cce45ce100581c5c37302e6cabd72e510524b
AzizbekEngineer <azizbekengineer@gmail.com>
2024-04-30 00:12:18 +0500

    filter

--- js/cards.js
+++ js/cards.js
@@ -5,14 +5,52 @@ let wrapper = document.querySelector('.products__cards')
 
 
 
-async function fetchData(URL, searchText = "") {
-    const data = await fetch(`${URL}/products?name=${searchText}`)
+async function fetchData(URL, searchText = "", categoryText = "") {
+    const data = await fetch(`${URL}/products?name=${searchText}&category=${categoryText}`)
     data
         .json()
         .then(res => createCard(res))
         .catch(err => console.log(err))
 }
 fetchData(API_URL)
+
+async function getCategory(URL) {
+    let data = await fetch(URL + "/products")
+    let categories = []
+
+
+    data
+        .json()
+        .then((res) => {
+            res.forEach((product) => {
+                !categories.includes(product.category)
+                    ? categories.push(product.category)
+                    : "";
+            })
+            mapCategory(categories)
+        })
+        .catch(err => console.log(err))
+}
+
+let selectCategory = document.querySelector('#products__categories')
+function mapCategory(categoryData) {
+    console.log(selectCategory);
+    let options = `<option value="">All</option>`
+    categoryData.forEach((category) => {
+        options += `
+            <option value="${category.toLowerCase()}">${category.slice(0, 1).toUpperCase() + category.slice(1).toLowerCase()}</option>
+`
+    })
+    selectCategory.innerHTML = options
+}
+
+selectCategory.addEventListener('change', (e) => {
+    let category = e.target.value
+    fetchData(API_URL, "", category)
+
+})
+
+getCategory(API_URL)
 
 function createCard(data) {
     let cards = ''
@@ -33,12 +71,16 @@ function createCard(data) {
     });
     wrapper.innerHTML = cards
 }
+
+
 wrapper.addEventListener('click', (e) => {
     if (e.target.className === "seeMore") {
         let id = e.target.dataset.id
         window.open(`./pages/product.html?productId=${id}`, '_self')
     }
 })
+
+
 productSearch.addEventListener('input', (e) => {
     let searchText = e.target.value
     fetchData(API_URL, searchText)
